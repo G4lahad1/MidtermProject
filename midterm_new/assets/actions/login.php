c7890689a40f6b0d14f78e857550c9c732d76ff0
cedricnathanpalo <cedricnathan.palohau@gmail.com>
2026-02-18 00:59:18 +0800

    RE: Updated login.php spacing format and sql execution
    Changed spacing format and used prepare function for SQL query to prevent SQL injections

--- midterm_new/assets/actions/login.php
+++ midterm_new/assets/actions/login.php
@@ -9,9 +9,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $conn->real_escape_string($_POST['username']);
     $pass = $_POST['password'];
 
-
-    $sql = "SELECT id, username, password, full_name, profile_image FROM users WHERE username = '$user'";
-    $result = $conn->query($sql);
+    // Prepare execute method
+    $sql = "SELECT id, username, password, full_name, profile_image FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $user);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows == 1) {
         // User found, now check password
@@ -28,17 +31,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: ../../dashboard.php");
             exit;
             
-        } else {
+        } 
+
+        else {
             // Password Incorrect
             header("Location: ../../index.php?error=invalid_password");
             exit;
         }
-    } else {
+    } 
+
+    else {
         // User not found
         header("Location: ../../index.php?error=user_not_found");
         exit;
     }
     
+    $stmt->close();
     $conn->close();
 }
 
