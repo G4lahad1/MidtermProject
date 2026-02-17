6bb43c65d0e01bdb35bdcb39ddac69bbe4b08a7b
cedricnathanpalo <cedricnathan.palohau@gmail.com>
2026-02-18 00:37:39 +0800

    Update login.php format and changed the sql execution
    Spacing changes and changed sql execution to prepare the statement to prevent any sql injections on $user

--- midterm_new/assets/actions/login.php
+++ midterm_new/assets/actions/login.php
@@ -3,26 +3,21 @@ session_start();
 
 include '../includes/db_connection.php'; 
 
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
-    
     // Sanitize input to prevent basic SQL injection
     $user = $conn->real_escape_string($_POST['username']);
     $pass = $_POST['password'];
 
 
-$sql = "SELECT id, username, password, full_name, profile_image FROM users WHERE username = '$user'";
+    $sql = "SELECT id, username, password, full_name, profile_image FROM users WHERE username = '$user'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
         // User found, now check password
         $row = $result->fetch_assoc();
         
-
         if ($pass === $row['password']) {
-            
             // Password Correct: Start Session
             $_SESSION['profile_image'] = $row['profile_image'];
             $_SESSION['loggedin'] = true;
@@ -46,4 +41,5 @@ $sql = "SELECT id, username, password, full_name, profile_image FROM users WHERE
     
     $conn->close();
 }
+
 ?>
